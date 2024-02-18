83deeb45f77039584fe82d34fb01538dfc42418d
Herocraft321 <israelisrael219@gmail.com>
2024-02-18 21:09:40 +0100

    version inicial

---
+++ resource/OLEDB-Runner/OLEDB-Runner/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace OLEDB_Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(args[0]))
+            {
+                throw new ArgumentException("Se necesita especificar el parametro 'Connection String'", "Connection String");
+            }
+
+            if (string.IsNullOrEmpty(args[1]))
+            {
+                throw new ArgumentException("Se necesita especificar el parametro 'Query'", "Query");
+            }
+
+            DataTable dt = new DataTable();
+            OleDbConnection oledbConnection = new OleDbConnection(args[0]);
+
+            oledbConnection.Open();
+
+            if (args[1].ToLower().Trim().StartsWith("select"))
+            {
+                OleDbDataAdapter adapter = new OleDbDataAdapter(args[1], oledbConnection);
+                DataSet dataSet = new DataSet();
+
+                adapter.Fill(dataSet);
+                dt = dataSet.Tables[0];
+            }
+            else
+            {
+                OleDbCommand comand = new OleDbCommand(args[1], oledbConnection);
+                comand.ExecuteNonQuery();
+            }
+            string html = @"<style>body {text-align: center;}table {margin: 2% auto;}table,th,td {border-collapse: collapse;padding: 10px;}</style><table border='1'>";
+            //add header row
+            html += "<tr>";
+            for (int i = 0; i < dt.Columns.Count; i++)
+                html += "<th style='font-size:30px'>" + dt.Columns[i].ColumnName + "</th>";
+            html += "</tr>";
+            //add rows
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                html += "<tr>";
+                for (int j = 0; j < dt.Columns.Count; j++)
+                    html += "<td style='font-size:15px'>" + dt.Rows[i][j].ToString() + "</td>";
+                html += "</tr>";
+            }
+            html += "</table>";
+            
+            Console.WriteLine(html);
+            
+        }
+    }
+}
---
+++ src/extension.ts
@@ -0,0 +1,97 @@
+import * as vscode from 'vscode';
+const fs = require('node:fs');
+const { spawn } = require('child_process'); 
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "oledb-runner" is now active!');
+	let connectionString = '';
+	
+
+	let disposable = vscode.commands.registerCommand('oledb-runner.configurarRuta', async () => {	
+		const editor = vscode.window.activeTextEditor;
+		const selectedText = editor?.document.getText(editor.selection);
+		const defDelimitar = ',';
+
+		const inputPath = await vscode.window.showInputBox({
+			placeHolder: "Escribe la ruta del directorio",
+			prompt: "Guarda el directorio donde se usara OLEDB.",
+			value: selectedText
+		});
+
+		if (inputPath === undefined){
+			console.log('Salir');
+			return;	
+		}
+
+		if (inputPath === '' || !fs.existsSync(inputPath?.replace(/"/gi,''))) {
+			console.log(inputPath);
+			vscode.window.showErrorMessage('La ruta expecificada no existe. Porfavor introduce una ruta valida.');
+			vscode.commands.executeCommand('oledb-runner.configurarRuta');
+			return;			
+		}
+		
+		const delimiter = await vscode.window.showInputBox({
+			placeHolder: "Escribe el delimitador de los CSV.",
+			prompt: "Determina el delimitador de los ficheros CSV. Por defecto ','",
+			value: selectedText
+		});
+
+		if (delimiter === undefined){
+			console.log('Delimitador por defecto');	
+		}
+
+		/**
+		 * ! Provider=Microsoft.ACE.OLEDB.12.0;Data Source="{Ruta}"; Extended Properties="text;HDR=Yes;CharacterSet=65001;FMT=Delimited(,)"; 
+		*/
+		connectionString = 'Provider=Microsoft.ACE.OLEDB.12.0;Data Source="' + inputPath + '"; Extended Properties="text;HDR=Yes;CharacterSet=65001;FMT=Delimited('+ (delimiter || defDelimitar) + ')";';
+		console.log(connectionString);
+	});
+
+	let disposable2 = vscode.commands.registerCommand('oledb-runner.Ejecutar', async () => {
+		const runner = '.\\resources\\Runner\\OLEDB-Runner.exe';
+		const editor = vscode.window.activeTextEditor;
+		const selectedText = editor?.document.getText(editor.selection);
+		const allText = editor?.document.getText();
+		let query = selectedText || allText;
+		
+		if (connectionString === ''){
+			vscode.window.showErrorMessage('Porfavor introduce una ruta para poder ejecutar la consulta. Ejcuta el comando "Configurar Ruta - OLEDB"');
+			return;
+		}
+		console.log(connectionString);
+		console.log(query);
+	
+		const process = spawn(runner, [connectionString,query]); 
+		process.stdout.on('data', (data: any) => { 
+			const panel = vscode.window.createWebviewPanel(
+				'viewTable',
+				'OLEBD',
+				vscode.ViewColumn.Two,
+				{
+					retainContextWhenHidden:true,
+					enableScripts:true
+				}
+			);
+
+			panel.webview.html = `${data}`;
+			console.log(`Output: ${data}`); 
+		}); 
+		
+		process.stderr.on('data', (data: any) => { 
+			vscode.window.showErrorMessage(`Error: ${data}`);
+			console.error(`Error: ${data}`); 
+		}); 
+		
+		process.on('close', (code: any) => { 
+			console.log(`Child process exited with code ${code}`); 
+		}); 
+	});
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
